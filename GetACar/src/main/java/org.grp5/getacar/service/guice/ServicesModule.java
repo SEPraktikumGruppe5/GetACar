7ac598e4fee0d5abd6874534aaa95817dd0dcafb
Alex <opncow@googlemail.com>
2013-11-27 21:00:57 +0100

    Forgot to bind TimeManagerImpl in Guice module! Srz peeps

--- GetACar/src/main/java/org.grp5/getacar/service/guice/ServicesModule.java
+++ GetACar/src/main/java/org.grp5/getacar/service/guice/ServicesModule.java
@@ -1,6 +1,8 @@
 package org.grp5.getacar.service.guice;
 
 import com.google.inject.AbstractModule;
+import org.grp5.getacar.service.TimeManager;
+import org.grp5.getacar.service.TimeManagerImpl;
 
 /**
  * Contains the bindings of the services.
@@ -9,5 +11,6 @@ public class ServicesModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(TimeManager.class).to(TimeManagerImpl.class);
     }
 }
